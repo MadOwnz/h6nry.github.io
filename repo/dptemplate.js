4e1ee6a57c3e3013360f9bf827133cdedb33bc7a
MadOwnz <ecbertram@gmail.com>
2018-02-24 10:17:16 -0500

    Update dptemplate.js

--- repo/dptemplate.js
+++ repo/dptemplate.js
@@ -24,9 +24,9 @@ var template = ' \
 ' + content + '\
 </div> \
 <hr> \
-<h3><a href="https://twitter.com/H6nry_/" target="_blank">Me on Twitter: H6nry_</a><br> \
-<a href="https://github.com/H6nry/" target="_blank">Me on Github: H6nry</a><br> \
-<a href="http://h6nry.github.io/" target="_blank">My Website</a></h3> \
+<h3><a href="https://twitter.com/ErnestBertram3/" target="_blank">Me on Twitter: MadOwnz</a><br> \
+<a href="https://github.com/MadOwnz/" target="_blank">Me on Github: MadOwnz</a><br> \
+<a href="http://madownz.github.io/" target="_blank">My Website</a></h3> \
 </td> \
 \
 </tr> \
